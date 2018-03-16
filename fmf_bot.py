cd23bf2767637fac0548477090839f5b2edd7197
Rytikov Maksim <tayamarn@yandex-team.ru>
2018-03-16 23:14:05 +0300

    sql

--- fmf_bot.py
+++ fmf_bot.py
@@ -1,45 +1,112 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import os
+import sqlite3
 import time
+
 import telepot
 from telepot.loop import MessageLoop
 
 OWN_NAME = 'tayamarn_reactor_bot'
-members_by_id = {}
-members_by_name = {}
-chat_by_id = {}
-matches = {}
 
 
-def add_member(member_id, member_name):
-    members_by_id[member_id] = member_name
-    members_by_name[member_name] = member_id
-    matches[member_id] = set([])
+def member_in_db(connection, member_id):
+    cur = connection.cursor()
+    cur.execute('SELECT COUNT(*) FROM members WHERE id=?', (member_id,))
+    return cur.fetchone()[0] > 0
 
 
-def list_message(member_id):
-    if not matches[member_id]:
+def add_member(connection, member_id, member_name, chat_id):
+    cur = connection.cursor()
+    cur.execute('INSERT INTO members VALUES (?, ?, ?)',
+                (member_id, member_name, chat_id))
+    connection.commit()
+
+
+def member_changed_name(connection, member_id, member_name):
+    cur = connection.cursor()
+    cur.execute('SELECT COUNT(*) FROM members WHERE id=? AND name=?',
+                (member_id, member_name))
+    return cur.fetchone()[0] == 0
+
+
+def update_name(connection, member_id, member_name):
+    cur = connection.cursor()
+    cur.execute('UPDATE members SET (?) WHERE id=?',
+                (member_name, member_id))
+    connection.commit()
+
+
+def member_likes(connection, member_id):
+    cur = connection.cursor()
+    cur.execute('SELECT match_name FROM matches WHERE member_id=?',
+                (member_id,))
+    return [c[0] for c in cur.fetchall()]
+
+
+def likes_message(connection, member_id):
+    likes = member_likes(connection, member_id)
+    if not likes:
         return 'Вы пока никого не добавили в список.'
-    return 'Ваш список: {}'.format(
-        ', '.join(sorted(matches[member_id])))
+    return 'Ваш список: {}'.format(', '.join(sorted(likes)))
 
 
-def is_match(member_id, name):
-    if name not in members_by_name:
-        return False
-    return members_by_id[member_id] in matches[members_by_name[name]]
+def member_matches(connection, member_id):
+    cur = connection.cursor()
+    cur.execute('''
+        SELECT m1.match_name FROM matches as m1
+        JOIN members as mem1 on m1.member_id = mem1.id
+        JOIN matches as m2 ON mem1.name = m2.match_name
+        JOIN members as mem2 on m2.member_id = mem2.id
+        WHERE m1.member_id=? AND m1.match_name = mem2.name''',
+                (member_id,))
+    return [c[0] for c in cur.fetchall()]
 
 
-def list_matches(member_id):
-    my_matches = []
-    for name in matches[member_id]:
-        if is_match(member_id, name):
-            my_matches.append(name)
-    if my_matches:
+def is_match(connection, member_id, name):
+    return name in member_matches(connection, member_id)
+
+
+def matches_message(connection, member_id):
+    matches = member_matches(connection, member_id)
+    if matches:
         return 'У вас взаимный интерес с этими людьми: {}'.format(
-            ', '.join(sorted(my_matches)))
+            ', '.join(sorted(matches)))
     else:
         return 'Пока у вас нет взаимного интереса ни с кем, но не сдавайтесь!'
+
+
+def add_match(connection, member_id, match_name):
+    cur = connection.cursor()
+    cur.execute('INSERT INTO matches VALUES (?, ?)',
+                (member_id, match_name))
+    connection.commit()
+
+
+def remove_match(connection, member_id, match_name):
+    cur = connection.cursor()
+    cur.execute('DELETE FROM matches WHERE member_id=? AND match_name=?',
+                (member_id, match_name))
+    connection.commit()
+
+
+def congratulations_messages(connection, member_id, match):
+    cur = connection.cursor()
+    cur.execute('SELECT name, chat_id FROM members WHERE id=?',
+                (member_id,))
+    name, chat_id = cur.fetchone()[0]
+    bot.sendMessage(chat_id, 'У вас совпадение с {}. Удачи!'.format(match))
+    cur.execute('SELECT chat_id FROM members WHERE name=?',
+                (match,))
+    chat_id = cur.fetchone()[0]
+    bot.sendMessage(chat_id, 'У вас совпадение с {}. Удачи!'.format(name))
+
+
+def check_new_matches(connection, member_id, new_matches):
+    matches = member_matches(connection, member_id)
+    for match in new_matches:
+        if match in matches:
+            congratulations_messages(connection, member_id, match)
 
 
 def handle(msg):
@@ -47,23 +114,31 @@ def handle(msg):
     command = msg['text']
     member_id = msg['from']['id']
     member_name = msg['from']['username']
+    db_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                           'fmf.db')
+    connection = sqlite3.connect(db_path)
 
-    if member_id not in members_by_id:
-        add_member(member_id, member_name)
+    if not member_in_db(connection, member_id):
+        add_member(connection, member_id, member_name, chat_id)
+    elif member_changed_name(connection, member_id, member_name):
+        update_name(connection, member_id, member_name)
 
     if command.startswith('/add '):
         if OWN_NAME in command[len('/add '):].split(' '):
             bot.sendMessage(chat_id, 'Это так неожиданно! 😘')
-        matches[member_id].update(command[len('/add '):].split(' '))
-        bot.sendMessage(chat_id, list_message(member_id))
+        new_matches = command[len('/add '):].split(' ')
+        for match_name in new_matches:
+            add_match(connection, member_id, match_name)
+        check_new_matches(connection, member_id, new_matches)
+        bot.sendMessage(chat_id, likes_message(connection, member_id))
     elif command.startswith('/remove '):
         for name in command[len('/remove '):].split(' '):
-            matches[member_id].discard(name)
-        bot.sendMessage(chat_id, list_message(member_id))
+            remove_match(connection, member_id, match_name)
+        bot.sendMessage(chat_id, likes_message(connection, member_id))
     elif command.startswith('/list'):
-        bot.sendMessage(chat_id, list_message(member_id))
+        bot.sendMessage(chat_id, likes_message(connection, member_id))
     elif command.startswith('/matches'):
-        bot.sendMessage(chat_id, list_matches(member_id))
+        bot.sendMessage(chat_id, matches_message(member_id))
     else:
         bot.sendMessage(chat_id, 'Я не знаю такой команды')
 
