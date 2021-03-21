11e228cd5d56894377242073deeb7d24d1b0abcd
Maksim Rytikov <mrytikov@networkoptix.com>
2021-03-21 18:11:43 +0300

    Add  a slightly better help

--- fmf_bot.py
+++ fmf_bot.py
@@ -293,7 +293,8 @@ async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+    await message.reply(HELP_MESSAGE.format('placeholder'))
+    # await message.reply(HELP_MESSAGE.format(command_parser.getHelp()))
 
 
 @dp.message_handler()
